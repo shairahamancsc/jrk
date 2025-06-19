f14be9c02a1fba9bb2267983d0d39a1911409ee3
Shaik Anisul Rahaman <shairahaman.csc@gmail.com>
2025-06-19 12:30:23 +0000

    Make whole UI Soft

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
@@ -9,7 +10,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border bg-card text-card-foreground shadow-lg", // Changed from shadow-sm to shadow-lg
       className
     )}
     {...props}
@@ -77,3 +78,4 @@ const CardFooter = React.forwardRef<
 CardFooter.displayName = "CardFooter"
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+
